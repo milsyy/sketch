bd2b99b2867a6411346e83652bb0a80b4109c9ae
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-01 16:27:02 -0500

    Added color input and button toggling

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container-two");
+const topButtons = document.querySelector(".top-buttons");
+const allButtons = document.querySelectorAll("button");
 
 const boardSize = 25;
 
@@ -9,3 +11,12 @@ for (let i = 0; i < boardSize; i++) {
   newDiv.style.width = 200 / Math.sqrt(boardSize) + "px";
   container.appendChild(newDiv);
 }
+
+const changeButtons = (e) => {
+  allButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  e.target.classList.add("active");
+};
+
+topButtons.addEventListener("click", changeButtons);
