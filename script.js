14ae0db2bdc06a4775c60510c435b41e86a557ff
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-02 06:04:58 -0500

    updated slider to be responsive with board size

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const container = document.querySelector(".container-two");
 const topButtons = document.querySelector(".top-buttons");
 const allButtons = document.querySelectorAll("button");
+const slider = document.querySelector("#slider");
 
-const boardSize = 25;
+let boardSize;
 
-for (let i = 0; i < boardSize; i++) {
-  newDiv = document.createElement("div");
-  newDiv.className = "board";
-  newDiv.style.flexBasis = 100 / Math.sqrt(boardSize) + "%";
-  newDiv.style.width = 200 / Math.sqrt(boardSize) + "px";
-  container.appendChild(newDiv);
-}
+const sliderVal = (e) => {
+  console.log(e.target.value);
+  boardSize = Math.floor(Math.sqrt(e.target.value));
+  console.log(boardSize);
+
+  for (let i = 0; i < boardSize * boardSize; i++) {
+    newDiv = document.createElement("div");
+    newDiv.className = "board";
+    newDiv.style.flexBasis = 100 / boardSize + "%";
+    newDiv.style.width = 550 / boardSize + "px";
+    newDiv.style.height = 550 / boardSize + "px";
+    container.appendChild(newDiv);
+  }
+};
+
+slider.addEventListener("mouseup", sliderVal);
 
 const changeButtons = (e) => {
   allButtons.forEach((button) => {
