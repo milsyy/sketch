dbc2c266e209bca02c0a5770033217c19cd7a4ec
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-02 07:34:54 -0500

    fixed colour selection bug when clicking parent

--- script.js
+++ script.js
@@ -2,31 +2,54 @@ const container = document.querySelector(".container-two");
 const topButtons = document.querySelector(".top-buttons");
 const allButtons = document.querySelectorAll("button");
 const slider = document.querySelector("#slider");
+const boardSliderText = document.querySelector("#board-size");
 
-let boardSize;
+let boardSize = 256;
 
-const sliderVal = (e) => {
-  console.log(e.target.value);
-  boardSize = Math.floor(Math.sqrt(e.target.value));
-  console.log(boardSize);
-
-  for (let i = 0; i < boardSize * boardSize; i++) {
+const boardOnLoad = (e) => {
+  for (let i = 0; i < boardSize; i++) {
     newDiv = document.createElement("div");
     newDiv.className = "board";
-    newDiv.style.flexBasis = 100 / boardSize + "%";
-    newDiv.style.width = 550 / boardSize + "px";
-    newDiv.style.height = 550 / boardSize + "px";
+    newDiv.style.flexBasis = 100 / Math.sqrt(boardSize) + "%";
+    newDiv.style.width = 550 / Math.sqrt(boardSize) + "px";
+    newDiv.style.height = 550 / Math.sqrt(boardSize) + "px";
     container.appendChild(newDiv);
   }
 };
 
-slider.addEventListener("mouseup", sliderVal);
-
 const changeButtons = (e) => {
+  if (e.target.classList.contains("top-buttons")) {
+    return;
+  }
+
   allButtons.forEach((button) => {
     button.classList.remove("active");
   });
   e.target.classList.add("active");
 };
 
+const sliderVal = (e) => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  boardSize = e.target.value * e.target.value;
+
+  for (let i = 0; i < boardSize; i++) {
+    newDiv = document.createElement("div");
+    newDiv.className = "board";
+    newDiv.style.flexBasis = 100 / Math.sqrt(boardSize) + "%";
+    newDiv.style.width = 550 / Math.sqrt(boardSize) + "px";
+    newDiv.style.height = 550 / Math.sqrt(boardSize) + "px";
+    container.appendChild(newDiv);
+  }
+};
+
+const sliderText = (e) => {
+  boardSliderText.textContent = `${e.target.value} x 
+  ${e.target.value}`;
+};
+
+window.addEventListener("load", boardOnLoad);
+slider.addEventListener("input", sliderText);
+slider.addEventListener("mouseup", sliderVal);
 topButtons.addEventListener("click", changeButtons);
