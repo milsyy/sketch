76f34e1529ab089921eabd0259f976e8969f5259
milsyy <128106812+milsyy@users.noreply.github.com>
2024-02-03 13:47:23 -0500

    Changed flex basis to a number from a percent

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const sliderVal = (e) => {
   for (let i = 0; i < boardSize; i++) {
     newDiv = document.createElement("div");
     newDiv.className = "board";
-    newDiv.style.flexBasis = 100 / Math.sqrt(boardSize) + "%";
+    newDiv.style.flexBasis = Math.sqrt(boardSize);
     newDiv.style.width = 550 / Math.sqrt(boardSize) + "px";
     newDiv.style.height = 550 / Math.sqrt(boardSize) + "px";
     container.appendChild(newDiv);
